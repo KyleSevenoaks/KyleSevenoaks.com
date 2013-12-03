2cab29a950c044ffaaf262063a75dee1953f7246
Kyle Sevenoaks <kylesevenoaks@gmail.com>
2013-12-03 21:59:17 +0100

    tons of changes to content, mobile layout and stuff
    STUUFFF

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,26 @@ $(function(){
 	$(".resolicious").on("click", function() {
 		window.location = "http://kyle_sevenoaks.bitbucket.org";
 	});
-
+	
+	$(".darkspawn").on("click", function() {
+		window.location = "http://d-spawn.com";
+	});
+	
+	$(".sickbass").on("click", function() {
+		window.location = "http://sickbassists.com";
+	});
+	
+	$(".CUimportant").on("click", function() {
+		window.location = "http://codeumbra.eu/why-you-should-use-important-as-a-last-resort";
+	});
+	
+	$(".CUcascade").on("click", function() {
+		window.location = "http://codeumbra.eu/the-cascade-part-of-css";
+	});
+	
+	$(".CUpureclick").on("click", function() {
+		window.location = "http://codeumbra.eu/pure-css-click-dropdown";
+	});
 
 
 	setTimeout(function () {
